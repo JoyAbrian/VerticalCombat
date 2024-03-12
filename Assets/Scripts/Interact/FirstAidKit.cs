d657ddee7649e8a2532f1e7b590c72055e5ed33a
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-12 17:53:02 +0800

    first aid kit interact

---
+++ Assets/Scripts/Interact/FirstAidKit.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAidKit : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Design the interaction using code
+    protected override void Interact()
+    {
+        Debug.Log("Interact with " + gameObject.name);
+    }
+}
