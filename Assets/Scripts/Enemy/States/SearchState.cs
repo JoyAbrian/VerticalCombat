1dcd813d724dadfcd8db52dd00978c3ee56a0f58
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-15 18:04:14 +0800

    search state script

---
+++ Assets/Scripts/Enemy/States/SearchState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchState : BaseStates
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Performed()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
